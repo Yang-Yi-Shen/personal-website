8aea3c612df2aa8e8042dcc9f03d8b8436104e7d
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-09-25 15:16:38 +0800

    feat: created introduction (with blank image)

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,5 +1,27 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <section id="introduction">
+        <div class="intro-text">
+            <h1 class="heading">Yang Yi Shen</h1>
+            <p>Frontend web developer</p>
+        </div>
+        <img class="intro-image" src="/src/assets/blank.jpg">
+    </section>
+</template>
 
-<style scoped></style>
+<style scoped>
+#introduction {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+    padding: 40px 0px 100px;
+}
+
+.intro-image {
+    height: 450px;
+    width: 278px;
+    object-fit: cover;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Introduction from '../components/Introduction.vue'
+import Description from '../components/Description.vue'
 </script>
 
 <template>
   <main>
-    
+    <Introduction />
   </main>
 </template>
