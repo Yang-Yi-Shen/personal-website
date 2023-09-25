a7d333f55dc2a8634fa47cedb3a9aff658bd3c18
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-09-25 14:27:08 +0800

    feat: created header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,46 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <header>
+        <p class="logo heading">&lt;yyshen></p>
+        <ul class="header-contacts">
+            <li class="header-contacts-item"><a class="header-contacts-link" href="github.com"><i
+                        class="header-contacts-icon fa fa-github"></i></a></li>
+            <li class="header-contacts-item"><a class="header-contacts-link" href="github.com"><i
+                        class="header-contacts-icon fa fa-github"></i></a></li>
+            <li class="header-contacts-item"><a class="header-contacts-link" href="github.com"><i
+                        class="header-contacts-icon fa fa-github"></i></a></li>
+            <li class="header-contacts-item"><a class="header-contacts-link" href="github.com"><i
+                        class="header-contacts-icon fa fa-github"></i></a></li>
+        </ul>
+    </header>
+</template>
 
-<style scoped></style>
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 10%;
+}
+
+.logo {
+    font-size: 24px;
+    font-weight: 600;
+}
+
+.header-contacts {
+    padding-inline-start: 0px;
+    list-style: none;
+    display: flex;
+    gap: 15px;
+}
+
+.header-contacts-link {
+    text-decoration: none;
+    color: white;
+}
+
+.header-contacts-icon {
+    font-size: 36px;
+}</style>
