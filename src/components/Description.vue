e86e02519e18629e86b6e4f5664dd9b84b3f04cd
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-09-27 15:33:15 +0800

    feat: added picture to description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -2,11 +2,11 @@
 
 <template>
     <section id="description">
-        <img class="description-image" src="/src/assets/blank_black.jpg">
+        <img class="description-image" src="/src/assets/selfie.jpeg">
         <div class="description-text">
             <h1 class="heading description-title">Hello there! 👋</h1>
-            <p>My name is Yang Yi Shen. I'm 16 years old, hoomeschooled, and like to learn and create things. I currently specialize in frontend web development, although I plan to progress onto full-stack development in the future.</p>
-            <p>When I'm not making websites, I practice Brazillian Jiu-Jitsu, play the piano, and read a lot of books. I also do side projects that extend beyond the frontend from time to time.</p>
+            <p>My name is Yang Yi Shen. I'm 16 years old, homeschooled, and like to learn and create things. I currently specialize in frontend web development, although I plan to progress onto full-stack development in the future.</p>
+            <p>When I'm not making websites, I do Brazillian Jiu-Jitsu, play the piano, and read a lot of books. I also do side projects that extend beyond the frontend from time to time.</p>
             <p>I reside in the lovely port city of Kaohsiung, Taiwan, although I plan to study college in America in the future.</p>
         </div>
     </section>
