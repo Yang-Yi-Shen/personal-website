8cfc7fc1bedd86164fe64d6a70f0c4f4ace26f7b
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-10-26 14:35:11 +0800

    feat: make introduction mobile friendly

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -2,6 +2,7 @@
 
 <template>
     <section id="introduction">
+        <img class="intro-image-mobile" src="/saigon.jpg">
         <div class="intro-text">
             <h1 class="heading intro-title">Yang Yi Shen</h1>
             <p class="intro-subtitle">A Taiwanese frontend web developer</p>
@@ -35,5 +36,35 @@
     height: 432px;
     width: 300px;
     object-fit: cover;
+}
+
+.intro-image-mobile {
+    display: none;
+}
+
+@media (max-width: 768px) {
+    #introduction {
+        height: auto;
+        width: 100vw;
+        flex-direction: column;
+        justify-content: baseline;
+        align-items: baseline;
+        padding: 50px;
+        gap: 0px;
+    }
+
+    .intro-title {
+        white-space: nowrap;
+        width: calc(100vw - 100px);
+    }
+
+    .intro-image {
+        display: none;
+    }
+
+    .intro-image-mobile {
+        width: calc(100vw - 100px);
+        display: block;
+    }
 }
 </style>
